5684d535d3db067203807d5be9469f890456652f
Ku6epnec <ku6epnec@rambler.ru>
2022-07-11 16:59:15 +0300

    AddCatalog

---
+++ Assets/Scripts/JsonCatalog.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Catalog
+{
+    public int current;
+    public string item_id;
+}
+
+public class JsonCatalog : MonoBehaviour
+{
+    public void Start()
+    {
+        Catalog myCatalog = new Catalog();
+        myCatalog.current = 1;
+        myCatalog.item_id = "wooden_sword";
+
+        string jsonCatalog = JsonUtility.ToJson(myCatalog);
+        Debug.Log(jsonCatalog);
+
+        File.ReadAllText("C:/Unity/Stattles/Assets/title-1B50D-FirstCatalog.json");
+        File.WriteAllText("C:/Unity/Stattles/Assets/MyCatalog", jsonCatalog);
+
+        myCatalog = JsonUtility.FromJson<Catalog>(jsonCatalog);
+        Debug.Log(myCatalog);
+
+        Debug.Log(File.ReadAllText("C:/Unity/Stattles/Assets/title-1B50D-FirstCatalog.json"));
+    }
+}
--- Assets/Scripts/PlayFabAccountManager.cs
+++ Assets/Scripts/PlayFabAccountManager.cs
@@ -4,10 +4,13 @@ using TMPro;
 using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.IO;
 
 public class PlayFabAccountManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text _titleLabel;
+    [SerializeField] private TMP_Text _catalogLabel = null;
     [SerializeField] private Button _changeAccountButton;
     [SerializeField] private Slider _sliderLoadingProcess;
     [SerializeField] private TMP_Text _loadingValue;
@@ -21,6 +24,25 @@ public class PlayFabAccountManager : MonoBehaviour
         LoadingTimer();
         PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(), OnGetAccountSuccess, OnError);
         _changeAccountButton.onClick.AddListener(ChangeAccount);
+        PlayFabClientAPI.GetCatalogItems(new GetCatalogItemsRequest(), OnGetCatalogSuccess, OnError);
+    }
+
+    private void OnGetCatalogSuccess(GetCatalogItemsResult result)
+    {
+        ShowCatalog(result.Catalog);
+        Debug.Log("Complete load catalog!");
+    }
+
+    private void ShowCatalog(List<CatalogItem> catalog)
+    {
+        foreach (var item in catalog)
+        {
+            if (item.Bundle == null)
+            {
+                Debug.Log("item_id: " + item.ItemId);
+                _catalogLabel.text += "item_id: " + item.ItemId + "\n";
+            }
+        }
     }
 
     private void Update()
@@ -36,11 +58,13 @@ public class PlayFabAccountManager : MonoBehaviour
     private void OnGetAccountSuccess(GetAccountInfoResult result)
     {
         var accountInfo = result.AccountInfo;
-        _titleLabel.text = "Welcome " + accountInfo.Username + accountInfo.PlayFabId;
+        _titleLabel.text = "Welcome " + accountInfo.Username + "\n" + accountInfo.PlayFabId;
         _sliderLoadingProcess.value = _endTimer; 
         _timerStatus = false;
         _imageEndLoading.color = Color.magenta;
+        //_catalogLabel.text = "Catalog.data: " + File.ReadAllText("C:/Unity/Stattles/Assets/title-1B50D-FirstCatalog.json");
     }
+
 
     private void OnError(PlayFabError error)
     {
