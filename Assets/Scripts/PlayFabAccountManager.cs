9a4ef2f034e4b8254bc58c5b1b5e7bd08d1576f4
Ku6epnec <ku6epnec@rambler.ru>
2022-07-25 18:30:22 +0300

    Add Characters List

--- Assets/Scripts/PlayFabAccountManager.cs
+++ Assets/Scripts/PlayFabAccountManager.cs
@@ -5,8 +5,6 @@ using PlayFab;
 using PlayFab.ClientModels;
 using UnityEngine.UI;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
 public class PlayFabAccountManager : MonoBehaviour
 {
@@ -19,13 +17,23 @@ public class PlayFabAccountManager : MonoBehaviour
 
     [SerializeField] private GameObject _newCharacterCreatePanel;
     [SerializeField] private Button _createCharacterButton;
+    [SerializeField] private Button _fightCharacterButton;
     [SerializeField] TMP_InputField _inputField;
-    [SerializeField] private List<SlotCharacterWidget> _slots;
+    [SerializeField] private SlotCharacterWidget _characterSlot;
+    [SerializeField] private GameObject _conteinerLayout;
+    private List<SlotCharacterWidget> _slots = new List<SlotCharacterWidget>();
 
     private string _characterName;
 
     private int _endTimer = 100;
     private bool _timerStatus = true;
+
+    private int _minXP = 5;
+    private int _maxXP = 200;
+
+    private string _level;
+    private string _xp;
+    private string _gold;
 
     private void Start()
     {
@@ -34,13 +42,132 @@ public class PlayFabAccountManager : MonoBehaviour
         _changeAccountButton.onClick.AddListener(ChangeAccount);
         PlayFabClientAPI.GetCatalogItems(new GetCatalogItemsRequest(), OnGetCatalogSuccess, OnError);
 
-        foreach (var slot in _slots)
-        {
-            slot.SlotButton.onClick.AddListener(OpenPanerCreateCharacter);
-        }
-
         _createCharacterButton.onClick.AddListener(CreateCharacterWithItem);
+        _fightCharacterButton.onClick.AddListener(StartFightCharacter);
         _inputField.onValueChanged.AddListener(OnNameChange);
+
+        GenerateCharacterList();             
+    }
+
+    private void GenerateCharacterList()
+    {
+        PlayFabClientAPI.GetAllUsersCharacters(new ListUsersCharactersRequest(),
+               result =>
+               {
+                   Debug.Log("Characters count: " + result.Characters.Count);
+                   WithdrawCharacters(result.Characters);
+               }, OnError);
+    }
+
+    private void WithdrawCharacters(List<CharacterResult> characters)
+    {
+        Debug.Log("Всего персонажей! " + characters.Count);
+        Debug.Log("Всего слотов было! " + _slots.Count);
+        CreateSlotsList(characters);
+        for (int characterNumber = 0; characterNumber < characters.Count; characterNumber++)
+        {
+            Debug.Log("CharacterNumber: " + characterNumber);
+            Debug.Log("characters[characterNumber].CharacterId: " + characters[characterNumber].CharacterId);
+            DrawCharacter(characters, characterNumber);
+        }
+        Debug.Log("Всего слотов стало! " + _slots.Count);
+    }
+
+    private void CreateSlotsList(List<CharacterResult> characters)
+    {
+        for (int characterNumber = 0; characterNumber < characters.Count + 1; characterNumber++)
+        {
+            _slots.Add(_characterSlot);
+        }
+    }
+
+    private void DrawCharacter(List<CharacterResult> characters, int characterNumber)
+    {
+        PlayFabClientAPI.GetCharacterStatistics(new GetCharacterStatisticsRequest
+        {
+            CharacterId = characters[characterNumber].CharacterId
+        },
+                            result =>
+                            {
+                                _level = result.CharacterStatistics["Level"].ToString();
+                                _xp = result.CharacterStatistics["XP"].ToString();
+                                _gold = result.CharacterStatistics["Gold"].ToString();
+                                Debug.Log("Персонаж! " + characters[characterNumber].CharacterName);
+                                Debug.Log("Уровень: " + _level + ", Опыт: " + _xp + ", Золото: " + _gold);
+                                Debug.Log("_slots.Count: " + _slots.Count);
+                                Debug.Log("_slots[characterNumber] было: " + _slots[characterNumber]);
+                                _slots[characterNumber] = Instantiate(_characterSlot, _conteinerLayout.transform);
+                                Debug.Log("_slots[characterNumber] стало: " + _slots[characterNumber]);
+                                _slots[characterNumber].ShowInfoCharacterSlot(characters[characterNumber].CharacterName, _level, _xp, _gold);
+                            }, OnError);
+    }
+
+    private void StartFightCharacter()
+    {
+        PlayFabClientAPI.GetAllUsersCharacters(new ListUsersCharactersRequest(),
+           result =>
+           {
+               Debug.Log("Characters count: " + result.Characters.Count);
+               for (int characterNumber = 0; characterNumber < result.Characters.Count; characterNumber++)
+               {
+                   Debug.Log("CharacterNumber: " + characterNumber);
+                   Debug.Log("characters[characterNumber].CharacterId: " + result.Characters[characterNumber].CharacterId);
+                   FindCharacter(result.Characters, characterNumber);
+               }
+           }, OnError);
+    }
+
+    private void FindCharacter(List<CharacterResult> characters, int characterNumber)
+    {
+            PlayFabClientAPI.GetCharacterStatistics(new GetCharacterStatisticsRequest
+            {
+                CharacterId = characters[characterNumber].CharacterId
+            },
+            result =>
+            {
+                var level = result.CharacterStatistics["Level"].ToString();
+                var xp = result.CharacterStatistics["XP"].ToString();
+                var gold = result.CharacterStatistics["Gold"].ToString();
+
+                Debug.Log("Персонаж! " + characters[characterNumber].CharacterName);
+                Debug.Log("Уровень: " + level + ", Опыт: " + xp + ", Золото: " + gold);
+
+                if (characters[characterNumber].CharacterName == "qwerty")
+                {
+                    Debug.Log("Кверти, подтверждаю! " + characters[characterNumber].CharacterName);
+                    UpdateCharacterXP(characters[characterNumber], result.CharacterStatistics);
+                }
+                else
+                {
+                    Debug.Log("Не Кверти! подтверждаю, не получает опыт! " + characters[characterNumber].CharacterName);
+                }
+
+                _slots[characterNumber].ShowInfoCharacterSlot(characters[characterNumber].CharacterName, level, xp, gold);
+            }, OnError);
+    }
+
+    private void UpdateCharacterXP(CharacterResult character, Dictionary<string, int> characterStatistics)
+    {
+        PlayFabClientAPI.UpdateCharacterStatistics(new UpdateCharacterStatisticsRequest
+        {
+            CharacterId = character.CharacterId,
+            CharacterStatistics = new Dictionary<string, int>
+            {
+                {"Level", characterStatistics["Level"]},
+                {"XP", characterStatistics["XP"] + Random.Range(_minXP, _maxXP)},
+                {"Gold", characterStatistics["Gold"]}
+            }
+        }, result =>
+        {
+            foreach (var characterStatistic in characterStatistics)
+            {
+                if (characterStatistic.Key == "XP")
+                {
+                    Debug.Log("У персонажа: " + character + " итоговый опыт: " + characterStatistic);
+                }
+            }
+
+        }, OnError);
     }
 
     private void OnNameChange(string characterName)
@@ -69,6 +196,7 @@ public class PlayFabAccountManager : MonoBehaviour
             CharacterStatistics = new Dictionary<string, int>
             {
                 {"Level", 1},
+                {"XP", 0},
                 {"Gold", 0}
             }
         }, result =>
@@ -79,7 +207,7 @@ public class PlayFabAccountManager : MonoBehaviour
         }, OnError);
     }
 
-    private void OpenPanerCreateCharacter()
+    private void OpenPanelCreateCharacter()
     {
         _newCharacterCreatePanel.SetActive(true);
     }
@@ -95,11 +223,16 @@ public class PlayFabAccountManager : MonoBehaviour
             result =>
             {
                 Debug.Log("Character count: " + result.Characters.Count);
-                ShowInfoCharacter(result.Characters);
+                for (int characterNumber = 0; characterNumber < result.Characters.Count; characterNumber++)
+                {
+                    Debug.Log("CharacterNumber: " + characterNumber);
+                    Debug.Log("characters[characterNumber].CharacterId: " + result.Characters[characterNumber].CharacterId);
+                    ShowInfoCharacter(result.Characters, characterNumber);
+                }         
             }, OnError);
     }
 
-    private void ShowInfoCharacter(List<CharacterResult> characters)
+    private void ShowInfoCharacter(List<CharacterResult> characters, int characterNumber)
     {
         if (characters.Count == 0)
         {
@@ -112,14 +245,16 @@ public class PlayFabAccountManager : MonoBehaviour
         {
             PlayFabClientAPI.GetCharacterStatistics(new GetCharacterStatisticsRequest
             {
-                CharacterId = characters.First().CharacterId
+                CharacterId = characters[characterNumber].CharacterId
             },
             result =>
             {
                 var level = result.CharacterStatistics["Level"].ToString();
+                var xp = result.CharacterStatistics["XP"].ToString();
                 var gold = result.CharacterStatistics["Gold"].ToString();
 
-                _slots.First().ShowInfoCharacterSlot(characters.First().CharacterName, level, gold);
+                Debug.Log("Уровень, опыт, золото: " + level + xp + gold);
+                _slots[characterNumber].ShowInfoCharacterSlot(characters[characterNumber].CharacterName, level, xp, gold);
             }, OnError);
         }
         else
--- Assets/Scripts/UI/SlotCharacterWidget.cs
+++ Assets/Scripts/UI/SlotCharacterWidget.cs
@@ -9,18 +9,21 @@ public class SlotCharacterWidget : MonoBehaviour
     [SerializeField] private GameObject _infoCharacterSlot;
     [SerializeField] private TMP_Text _nameLabel;
     [SerializeField] private TMP_Text _levelLabel;
+    [SerializeField] private TMP_Text _xpLabel;
     [SerializeField] private TMP_Text _goldLabel;
 
     public Button SlotButton => _button;
     
-    public void ShowInfoCharacterSlot(string name, string level, string gold)
+    public void ShowInfoCharacterSlot(string name, string level, string xp, string gold)
     {
-        _nameLabel.text = name;
-        _levelLabel.text = level;
-        _goldLabel.text = gold;
-
         _infoCharacterSlot.SetActive(true);
         _emptySlot.SetActive(false);
+
+        _nameLabel.text = name;
+        _levelLabel.text = level;
+        _xpLabel.text = xp;
+        _goldLabel.text = gold;
+
     }
 
     public void ShowEmptySlot()
